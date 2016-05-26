3b0645e48fc8313c69c871264a67d24891e4cb81
Tj Carskadon <tjcarskadon@gmail.com>
2016-05-26 15:08:58 -0700

    manually fetch RSS from nyt and send to window

--- server/static/config/router.js
+++ server/static/config/router.js
@@ -1,7 +1,12 @@
-
+var fetch = require('../services/fetchRss.js');
 
 module.exports = (app) => {
+  
   app.get('/test', (req, res) => {
     res.send('made it yo!');
+  });
+
+  app.get('/pull', (req, res) => {
+    fetch('http://rss.nytimes.com/services/xml/rss/nyt/Politics.xml', req,res);
   });
 };
