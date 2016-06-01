e4ff1df1ed138345ea048af2ecb3a12909e67e39
Tj Carskadon <tjcarskadon@gmail.com>
2016-06-01 08:26:24 -0700

    remove import statements from index.js

--- client/app/index.js
+++ client/app/index.js
@@ -1,11 +1,12 @@
+<<<<<<< 8737d8fe75e37cc421b8995cd8e51319ec756236
 
 import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import { articles } from './articles'
-// //TODO: convert all jsx files to js files
 import { App } from './components/App';
+// //TODO: convert all jsx files to js files
 
 let createStore = Redux.createStore;
 
