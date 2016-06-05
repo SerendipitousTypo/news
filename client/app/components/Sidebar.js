3f600525eaf6c1e142c441247998153473759811
JonasMS <jonas.sota@gmail.com>
2016-06-05 11:03:40 -0700

    FEAT - Get Articles by Topic, Sidebar, WIP

--- client/app/components/Sidebar.js
+++ client/app/components/Sidebar.js
@@ -1,62 +1,72 @@
 import React from 'react'
+import { fetchArticles, setFilter } from '../actions'
 
-export var Sidebar = ({}) => (
-  <div>
-  <h3 className="topics-heading left-margin-fix">Topics</h3>
-  
-    <div className="demo-list-action mdl-list">
-  <div className="mdl-list__item">
-      <span className="mdl-list__item-primary-content">
-        <i className="material-icons mdl-list__item-avatar">account_balance</i>
-        <a href="#" className="main-link">
-          <span>Politics</span>
-        </a>
-      </span>
-  </div>
-  <div className="mdl-list__item">
-      <span className="mdl-list__item-primary-content">
-        <i className="material-icons mdl-list__item-avatar">assessment</i>
-        <a href="#" className="main-link">
-          <span>Economics</span>
-        </a>
-      </span>
-  </div>
-  <div className="mdl-list__item">
-      <span className="mdl-list__item-primary-content">
-        <i className="material-icons mdl-list__item-avatar">assignment_turned_in</i>
-        <a href="#" className="main-link">
-          <span>US elections</span>
-        </a>
-      </span>
-    <span className="mdl-list__item-secondary-content">
-      <a className="mdl-list__item-secondary-action" href="#"><i className="material-icons">star</i></a>
-    </span>
-  </div>
-  <div className="mdl-list__item">
-    <span className="mdl-list__item-primary-content">
-      <i className="material-icons mdl-list__item-avatar">whatshot</i>
-      <a href="#" className="main-link">
-        <span>Current events</span>
-      </a>
-    </span>
-    <span className="mdl-list__item-secondary-content">
-    </span>
-  </div>
-</div>
+export const Sidebar = ({store}) => {
+  let filter = store.getState().articleFilter;
+  let nextFilter = Object.assign({}, filter);
+  nextFilter.view = 'A_TOPIC';
+  nextFilter.type = 'TOPIC';
+  return (
 
+    <div>
+        <h3 className="topics-heading left-margin-fix">Topics</h3>
 
+        <div className="demo-list-action mdl-list">
+          <div className="mdl-list__item">
+            <span className="mdl-list__item-primary-content">
 
+              <a href="#" className="main-link">
+                <span onClick={() => {
+                    //change filter.topic to
+                    //'Politics'
+                    nextFilter.topic = 'Politics';
+                    store.dispatch(setFilter(
+                      nextFilter,
+                      'Politics'
+                    ));
 
+                    //fetch articles w/ topic 'Politics'
+                    store.dispatch(fetchArticles(
+                      nextFilter
+                    ));
+                  }
 
+                  }
+                >Politics</span>
+              </a>
 
+            </span>
+          </div>
+          <div className="mdl-list__item">
+              <span className="mdl-list__item-primary-content">
 
+                <a href="#" className="main-link">
+                  <span>Economics</span>
+                </a>
+              </span>
+          </div>
+          <div className="mdl-list__item">
+            <span className="mdl-list__item-primary-content">
 
+              <a href="#" className="main-link">
+                <span>US elections</span>
+              </a>
+            </span>
+          <span className="mdl-list__item-secondary-content">
+            <a className="mdl-list__item-secondary-action" href="#"></a>
+          </span>
+        </div>
+        <div className="mdl-list__item">
+          <span className="mdl-list__item-primary-content">
 
-
-
-  </div>
-);
-
-//TODO: Delete
-window.Sidebar = Sidebar;
-
+            <a href="#" className="main-link">
+              <span>Current events</span>
+            </a>
+          </span>
+          <span className="mdl-list__item-secondary-content">
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+};
