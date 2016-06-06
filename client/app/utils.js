0a0da298f362f05c6df1ee860ebb62196cddcd9e
JonasMS <jonas.sota@gmail.com>
2016-06-06 10:41:58 -0700

    REFAC - Refactor article sorting in populateRows, Main.js

--- client/app/utils.js
+++ client/app/utils.js
@@ -4,3 +4,16 @@
 export const sanitize = (string) => (
   string.replace(/_.*/, '')
 );
+
+
+export const sortArticle = (sorted, article, prop) => {
+  //make shallow copy for immutability
+  sorted = Object.assign({}, sorted)
+
+
+  sorted.hasOwnProperty(prop) ?
+    sorted[prop].push(article) :
+    sorted[prop] = [article];
+
+  return sorted;
+}
