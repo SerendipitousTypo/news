897ca4f586ca2dc02adfb4395448b36396148ca6
Shams Ali <ali.s90@outlook.com>
2016-06-08 12:06:02 -0700

    graph is on same page of article

--- client/app/components/ArticleEntry.js
+++ client/app/components/ArticleEntry.js
@@ -26,52 +26,15 @@ export class ArticleEntry extends Component {
     };
     this.handleOpenDialog = this.handleOpenDialog.bind(this);
     this.handleCloseDialog = this.handleCloseDialog.bind(this);
-    this.handleAnalyze = this.handleAnalyze.bind(this);
   }
 
-  handleOpenDialog() {
-    this.setState({
-      openDialog: true
-    });
-    var context = this;
-    //make request, then change the state of modal text
-    fetch("http://localhost:3000/v1/pages?url=" + this.props.article.url)
-    .then( function(text) {
-      return text.json();
-    })
-    .then(function(text) {
-      console.log('formated text', text.data[0].text);
-      return context.setState({
-        modalText:  text.data[0].text.split('\n').map(function(item) {
-                        return (
-                          <p className="article-paragraph">
-                            {item}
-                            <br/>
-                          </p>
-                        )
-                      })
-      })
-    })
-    .catch(function(e) {
-      context.setState({
-        modalText:  <div className="error-message">
-                      <div className="error-icon-wrapper">
-                        <i className="material-icons error-icon">error_outline</i>
-                      </div>
-                      Sorry, the article is not available at this time. You can view source here:
-                      <Button colored raised className="error-btn">
-                        <a href={context.props.article.url} target="_blank" className="error-link">View source</a>
-                      </Button>
-                    </div>
-      })
-    });
-  }
   //return fetch("http://localhost:3000/v1/pages?text=" + text)
-  handleAnalyze() {
-    var hello = 'hello'
+  handleOpenDialog() {
+
     this.setState({
       openDialog: true
     });
+    var mainText = '';
     var context = this;
     //make request, then change the state of modal text
     fetch("http://localhost:3000/v1/pages?url=" + this.props.article.url)
@@ -79,9 +42,10 @@ export class ArticleEntry extends Component {
       return text.json();
     })
     .then(function(text){
-      var text = text.data[0].text
-      console.log('this is text', text);
-      return fetch("http://localhost:3000/v1/tone_analyzers?text=" + text)
+      mainText = text.data[0].text
+      text = text.data[0].text
+      console.log('this is text', mainText);
+      return fetch("http://localhost:3000/v1/tone_analyzers?text=" + text);
     })
     .then(function(data){
       return data.json();
@@ -97,17 +61,29 @@ export class ArticleEntry extends Component {
         emotion_Arr.push(obj);
       }
       console.log('this is emotion arr: ', emotion_Arr);
+      console.log('this is maintext', mainText);
       context.setState({
         emotion_tone: update(context.state.emotion_tone, {
           watsonData: {$set: emotion_Arr}
         }),
       });
-      //emotion_Arr = JSON.stringify(emotion_Arr);
       return context.setState({
-        modalText:  <div className="article-paragraph">
-                      <Chart pieData={ context.state.emotion_tone }/>
-                      <br/>
-                    </div>
+        modalText:
+
+                          <div>
+                            <div className="article-paragraph">
+                              <Chart pieData={ context.state.emotion_tone }/>
+                              <br/>
+                            </div>
+                            <p className="article-paragraph">
+                              {mainText}
+                              <br/>
+                            </p>
+                          </div>
+
+
+
+
       })
     })
     .catch(function(e) {
@@ -117,6 +93,9 @@ export class ArticleEntry extends Component {
                         <i className="material-icons error-icon">error_outline</i>
                       </div>
                       Sorry, the article is not available at this time.
+                      <Button colored raised className="error-btn">
+                        <a href={context.props.article.url} target="_blank" className="error-link">View source</a>
+                      </Button>
                     </div>
       })
     });
@@ -146,7 +125,6 @@ export class ArticleEntry extends Component {
               <div className="card-text" onClick={ this.handleOpenDialog}>{this.props.article.content}</div>
               <div className="mdl-card__actions mdl-card--border">
                 <Button colored onClick={this.handleOpenDialog} raised ripple>Full article</Button>
-                <Button colored onClick={this.handleAnalyze} raised ripple>Analyze</Button>
                 <div className="url-link-btn"><a href={this.props.article.url} target="_blank" className="btn-link mdl-button mdl-js-button mdl-js-ripple-effect"><i className="material-icons">public</i></a></div>
 
                   <Dialog open={this.state.openDialog} className="article-dialog">
